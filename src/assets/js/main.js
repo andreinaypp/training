dd56dd43a0d8b74678f285618a6e6b19272859b4
andreinaypp <andreinaypp88@gmail.com>
2021-09-03 18:23:59 -0400

    usuarios

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,33 @@
+// ------------------------------------------------------------
+// MENU
+// ------------------------------------------------------------
+
 let open = document.querySelector("#open");
 let close = document.querySelector("#close");
 
-function abrirMenu() {
-  // document.querySelector(".menu-open").style.left = "0%";
-  alert("dando click");
-}
+open.addEventListener("click", () => {
+  document.querySelector(".menu-open").style.left = "0%";
+});
+close.addEventListener("click", () => {
+  document.querySelector(".menu-open").style.left = "-100%";
+});
 
-open.addEventListener("click", abrirMenu);
+// ------------------------------------------------------------
+// LOGIN
+// ------------------------------------------------------------
+
+let eyeHide = document.querySelector("#hide");
+let eyeShow = document.querySelector("#show");
+let password = document.querySelector("#password");
+
+show.addEventListener("click", () => {
+  password.setAttribute("type", "text");
+  eyeHide.style.display = "block";
+  eyeShow.style.display = "none";
+});
+
+hide.addEventListener("click", () => {
+  password.setAttribute("type", "password");
+  eyeHide.style.display = "none";
+  eyeShow.style.display = "block";
+});
